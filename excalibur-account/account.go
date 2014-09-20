6c9c17ac99cdebd5b38c4f1ace30bf93c1b54233
Putta Khunchalee <ultimaweapon@outlook.com>
2014-09-20 17:41:42 +0700

    Start project.

---
+++ excalibur-account/account.go
@@ -0,0 +1,43 @@
+////////////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2014 Putta Khunchalee.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+////////////////////////////////////////////////////////////////////////////////////
+package main
+
+import (
+	"encoding/xml"
+	"os"
+)
+
+type IPEndPoint struct {
+	XMLName xml.Name `xml:"address"`
+	Name    string   `xml:"name,attr"`
+	Port    int16    `xml:"port,attr"`
+}
+
+type Server struct {
+	XMLName   xml.Name     `xml:"server"`
+	Addresses []IPEndPoint `xml:"address"`
+}
+
+type Config struct {
+	XMLName xml.Name `xml:"excalibur-account"`
+	Server  Server   `xml:"server"`
+}
+
+func main() {
+	os.Exit(0)
+}
